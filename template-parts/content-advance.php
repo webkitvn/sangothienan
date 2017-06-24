ac07a0ced33c8f06739506fadabc1de71277c700
webkitvn <cuongphamquoc91@gmail.com>
2017-06-24 12:31:37 +0700

    Fix advances section

---
+++ template-parts/content-advance.php
@@ -0,0 +1,30 @@
+<section class="advances">
+	<div class="advances-wrapper">
+		<div class="row">
+			<div class="col s12 m6 l3">
+				<div class="item">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/icon-1.svg" alt="">
+					<h3>Nhập khẩu chính hãng từ Đức</h3>
+				</div>
+			</div>
+			<div class="col s12 m6 l3">
+				<div class="item">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/icon-2.svg" alt="">
+					<h3>Miễn phí giao hàng</h3>
+				</div>
+			</div>
+			<div class="col s12 m6 l3">
+				<div class="item">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/icon-3.svg" alt="">
+					<h3>Bảo hành trọn đời sản phẩm</h3>
+				</div>
+			</div>
+			<div class="col s12 m6 l3">
+				<div class="item">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/icon-4.svg" alt="">
+					<h3>Chứng nhận Quốc tế</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
